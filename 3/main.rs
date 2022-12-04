7f1497172bff1017bf663bc4f89330e4f4006710
Cameron Durham <camdurha@amazon.com>
2022-12-03 21:48:25 -0700

    Use include_str!()

--- 3/main.rs
+++ 3/main.rs
@@ -6,6 +6,28 @@ use std::path::Path;
 fn main() {
     // TODO: try to make this cleaner, more idiomatic
     let mut score = 0;
+    let content = include_str!("input.txt");
+    for line in content.split("\n") {
+        let chars: Vec<char> = line.chars().collect();
+        let midpoint = line.len() / 2;
+        let mut lcharset: HashSet<char> = HashSet::new();
+        for i in 0..midpoint {
+            lcharset.insert(chars.get(i).unwrap().clone());
+        }
+        let mut rcharset: HashSet<char> = HashSet::new();
+        for i in midpoint..line.len() {
+            rcharset.insert(chars.get(i).unwrap().clone());
+        }
+        let intersection: Vec<&char> = lcharset.intersection(&rcharset).collect();
+        if intersection.len() != 0 {
+            score += get_value(intersection.get(0).unwrap());
+        }
+    }
+    println!("the score is: {}", score);
+
+    let mut score = 0;
+
+    // old implementation
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
             let chars: Vec<char> = line.as_ref().unwrap().chars().collect();
@@ -27,14 +49,6 @@ fn main() {
     println!("the score is: {}", score);
 }
 
-// From: https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
 fn get_value(c: &char) -> i32 {
     let val = c.clone() as i32;
     if val >= 'a' as i32 && val <= 'z' as i32 {
@@ -44,4 +58,13 @@ fn get_value(c: &char) -> i32 {
     } else {
         return -1;
     }
+}
+
+// From: https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
